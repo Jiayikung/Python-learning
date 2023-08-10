d6efc9731b891f0b9d16f8b2ec0ef4f83fbc1908
DORIS <112765699+Jiayikung@users.noreply.github.com>
2023-08-10 02:20:23 -0400

    update better solution

--- blur.py
+++ blur.py
@@ -31,125 +31,156 @@ def blur(img):
     # Create a new blank img that is as big as the original one
     new_img = SimpleImage.blank(img.width, img.height)
 
+    """
+    Solution 1: Opt to use 4 for-loops
+    """
     # Loop over the picture
     for x in range(img.width):
         for y in range(img.height):
-            img_pixel = img.get_pixel(x, y)
+            r_sum = 0  # The sum of red bytes from designated pixels
+            g_sum = 0  # The sum of green bytes from designated pixels
+            b_sum = 0  # The sum of blue bytes from designated pixels
+            count = 0
+            for i in range(-1, 2, 1):
+                for j in range(-1, 2, 1):
+                    pixel_x = x + i
+                    pixel_y = y + j
+                    if 0 <= pixel_x < img.width:
+                        if 0 <= pixel_y < img.height:
+                            img_pixel = img.get_pixel(pixel_x, pixel_y)
+                            r_sum += img_pixel.red
+                            g_sum += img_pixel.green
+                            b_sum += img_pixel.blue
+                            count += 1
             new_img_pixel = new_img.get_pixel(x, y)
-            red1 = 0  # The sum of red bytes from designated pixels
-            green1 = 0  # The sum of green bytes from designated pixels
-            blue1 = 0  # The sum of blue bytes from designated pixels
-
-            # Belows are 9 conditions of pixel filling, depending on pixels' x,y orientation.
-            if x == 0 and y == 0:
-                # Get pixel at the top-left corner of the image.
-                for p in range(2):
-                    for q in range(2):
-                        img_pixel = img.get_pixel(p, q)
-                        red1 += img_pixel.red
-                        green1 += img_pixel.green
-                        blue1 += img_pixel.blue
-                new_img_pixel.red = red1//4
-                new_img_pixel.green = green1//4
-                new_img_pixel.blue = blue1//4
-
-            elif x == img.width-1 and y == 0:
-                # Get pixel at the top-right corner of the image.
-                for p in range(img.width-2, img.width):
-                    for q in range(2):
-                        img_pixel = img.get_pixel(p, q)
-                        red1 += img_pixel.red
-                        green1 += img_pixel.green
-                        blue1 += img_pixel.blue
-                new_img_pixel.red = red1//4
-                new_img_pixel.green = green1//4
-                new_img_pixel.blue = blue1//4
-
-            elif x == 0 and y == img.height-1:
-                # Get pixel at the bottom-left corner of the image
-                for p in range(2):
-                    for q in range(img.height-2, img.height):
-                        img_pixel = img.get_pixel(p, q)
-                        red1 += img_pixel.red
-                        green1 += img_pixel.green
-                        blue1 += img_pixel.blue
-                new_img_pixel.red = red1//4
-                new_img_pixel.green = green1//4
-                new_img_pixel.blue = blue1//4
-
-            elif x == img.width-1 and y == img.height-1:
-                # Get pixel at the bottom-right corner of the image
-                for p in range(img.width-2, img.width):
-                    for q in range(img.height-2, img.height):
-                        img_pixel = img.get_pixel(p, q)
-                        red1 += img_pixel.red
-                        green1 += img_pixel.green
-                        blue1 += img_pixel.blue
-                new_img_pixel.red = red1//4
-                new_img_pixel.green = green1//4
-                new_img_pixel.blue = blue1//4
- 
-            elif 0 < x < img.width-1 and y == 0:
-                # Get top edge's pixels (without two corners)
-                for p in range(x-1, x+2):
-                    for q in range(2):
-                        img_pixel = img.get_pixel(p, q)
-                        red1 += img_pixel.red
-                        green1 += img_pixel.green
-                        blue1 += img_pixel.blue
-                new_img_pixel.red = red1//6
-                new_img_pixel.green = green1//6
-                new_img_pixel.blue = blue1//6
-
-            elif 0 < x < img.width-1 and y == img.height-1:
-                # Get bottom edge's pixels (without two corners)
-                for p in range(x-1, x+2):
-                    for q in range(img.height-2, img.height):
-                        img_pixel = img.get_pixel(p, q)
-                        red1 += img_pixel.red
-                        green1 += img_pixel.green
-                        blue1 += img_pixel.blue
-                new_img_pixel.red = red1 // 6
-                new_img_pixel.green = green1 // 6
-                new_img_pixel.blue = blue1 // 6
-
-            elif x == 0 and 0 < y < img.height-1:
-                # Get left edge's pixels (without two corners)
-                for p in range(2):
-                    for q in range(y-1, y+2):
-                        img_pixel = img.get_pixel(p, q)
-                        red1 += img_pixel.red
-                        green1 += img_pixel.green
-                        blue1 += img_pixel.blue
-                new_img_pixel.red = red1 // 6
-                new_img_pixel.green = green1 // 6
-                new_img_pixel.blue = blue1 // 6
-
-            elif x == img.width-1 and 0 < y < img.height-1:
-                # Get right edge's pixels (without two corners)
-                for p in range(img.width-2, img.width):
-                    for q in range(y-1, y+2):
-                        img_pixel = img.get_pixel(p, q)
-                        red1 += img_pixel.red
-                        green1 += img_pixel.green
-                        blue1 += img_pixel.blue
-                new_img_pixel.red = red1 // 6
-                new_img_pixel.green = green1 // 6
-                new_img_pixel.blue = blue1 // 6
-
-            else:
-                # Inner pixels.
-                for p in range(x-1, x+2):
-                    for q in range(y-1, y+2):
-                        img_pixel = img.get_pixel(p, q)
-                        red1 += img_pixel.red
-                        green1 += img_pixel.green
-                        blue1 += img_pixel.blue
-                new_img_pixel.red = red1 // 9
-                new_img_pixel.green = green1 // 9
-                new_img_pixel.blue = blue1 // 9
-
+            new_img_pixel.red = r_sum / count
+            new_img_pixel.green = g_sum / count
+            new_img_pixel.blue = b_sum / count
     return new_img
+
+
+    """
+    Solution 2: Divide into 9 conditions of pixel filling, resulting in long redundant codes
+    """
+    # # Loop over the picture
+    # for x in range(img.width):
+    #     for y in range(img.height):
+    #         img_pixel = img.get_pixel(x, y)
+    #         new_img_pixel = new_img.get_pixel(x, y)
+    #         red1 = 0  # The sum of red bytes from designated pixels
+    #         green1 = 0  # The sum of green bytes from designated pixels
+    #         blue1 = 0  # The sum of blue bytes from designated pixels
+    #
+    #         # Belows are 9 conditions of pixel filling, depending on pixels' x,y orientation.
+    #         if x == 0 and y == 0:
+    #             # Get pixel at the top-left corner of the image.
+    #             for p in range(2):
+    #                 for q in range(2):
+    #                     img_pixel = img.get_pixel(p, q)
+    #                     red1 += img_pixel.red
+    #                     green1 += img_pixel.green
+    #                     blue1 += img_pixel.blue
+    #             new_img_pixel.red = red1//4
+    #             new_img_pixel.green = green1//4
+    #             new_img_pixel.blue = blue1//4
+    #
+    #         elif x == img.width-1 and y == 0:
+    #             # Get pixel at the top-right corner of the image.
+    #             for p in range(img.width-2, img.width):
+    #                 for q in range(2):
+    #                     img_pixel = img.get_pixel(p, q)
+    #                     red1 += img_pixel.red
+    #                     green1 += img_pixel.green
+    #                     blue1 += img_pixel.blue
+    #             new_img_pixel.red = red1//4
+    #             new_img_pixel.green = green1//4
+    #             new_img_pixel.blue = blue1//4
+    #
+    #         elif x == 0 and y == img.height-1:
+    #             # Get pixel at the bottom-left corner of the image
+    #             for p in range(2):
+    #                 for q in range(img.height-2, img.height):
+    #                     img_pixel = img.get_pixel(p, q)
+    #                     red1 += img_pixel.red
+    #                     green1 += img_pixel.green
+    #                     blue1 += img_pixel.blue
+    #             new_img_pixel.red = red1//4
+    #             new_img_pixel.green = green1//4
+    #             new_img_pixel.blue = blue1//4
+    #
+    #         elif x == img.width-1 and y == img.height-1:
+    #             # Get pixel at the bottom-right corner of the image
+    #             for p in range(img.width-2, img.width):
+    #                 for q in range(img.height-2, img.height):
+    #                     img_pixel = img.get_pixel(p, q)
+    #                     red1 += img_pixel.red
+    #                     green1 += img_pixel.green
+    #                     blue1 += img_pixel.blue
+    #             new_img_pixel.red = red1//4
+    #             new_img_pixel.green = green1//4
+    #             new_img_pixel.blue = blue1//4
+    #
+    #         elif 0 < x < img.width-1 and y == 0:
+    #             # Get top edge's pixels (without two corners)
+    #             for p in range(x-1, x+2):
+    #                 for q in range(2):
+    #                     img_pixel = img.get_pixel(p, q)
+    #                     red1 += img_pixel.red
+    #                     green1 += img_pixel.green
+    #                     blue1 += img_pixel.blue
+    #             new_img_pixel.red = red1//6
+    #             new_img_pixel.green = green1//6
+    #             new_img_pixel.blue = blue1//6
+    #
+    #         elif 0 < x < img.width-1 and y == img.height-1:
+    #             # Get bottom edge's pixels (without two corners)
+    #             for p in range(x-1, x+2):
+    #                 for q in range(img.height-2, img.height):
+    #                     img_pixel = img.get_pixel(p, q)
+    #                     red1 += img_pixel.red
+    #                     green1 += img_pixel.green
+    #                     blue1 += img_pixel.blue
+    #             new_img_pixel.red = red1 // 6
+    #             new_img_pixel.green = green1 // 6
+    #             new_img_pixel.blue = blue1 // 6
+    #
+    #         elif x == 0 and 0 < y < img.height-1:
+    #             # Get left edge's pixels (without two corners)
+    #             for p in range(2):
+    #                 for q in range(y-1, y+2):
+    #                     img_pixel = img.get_pixel(p, q)
+    #                     red1 += img_pixel.red
+    #                     green1 += img_pixel.green
+    #                     blue1 += img_pixel.blue
+    #             new_img_pixel.red = red1 // 6
+    #             new_img_pixel.green = green1 // 6
+    #             new_img_pixel.blue = blue1 // 6
+    #
+    #         elif x == img.width-1 and 0 < y < img.height-1:
+    #             # Get right edge's pixels (without two corners)
+    #             for p in range(img.width-2, img.width):
+    #                 for q in range(y-1, y+2):
+    #                     img_pixel = img.get_pixel(p, q)
+    #                     red1 += img_pixel.red
+    #                     green1 += img_pixel.green
+    #                     blue1 += img_pixel.blue
+    #             new_img_pixel.red = red1 // 6
+    #             new_img_pixel.green = green1 // 6
+    #             new_img_pixel.blue = blue1 // 6
+    #
+    #         else:
+    #             # Inner pixels.
+    #             for p in range(x-1, x+2):
+    #                 for q in range(y-1, y+2):
+    #                     img_pixel = img.get_pixel(p, q)
+    #                     red1 += img_pixel.red
+    #                     green1 += img_pixel.green
+    #                     blue1 += img_pixel.blue
+    #             new_img_pixel.red = red1 // 9
+    #             new_img_pixel.green = green1 // 9
+    #             new_img_pixel.blue = blue1 // 9
+    #
+    # return new_img
 
 
 
